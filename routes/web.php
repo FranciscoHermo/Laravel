6b30747779c08b55c98c14af0ec5404c6e07d8e3
FranciscoHermo <francisco.hermosillo@cceo.com.mx>
2026-02-13 11:08:40 -0600

    Section 4

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Http\Controllers\PrimerControlador;
-use App\Http\Controllers\SegundoControlador;
 use Illuminate\Support\Facades\Route;
-use Illuminate\View\View;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('test',[PrimerControlador::class,'index']);
-Route::get('otro/{post}/{otro}',[PrimerControlador::class,'otro']);
