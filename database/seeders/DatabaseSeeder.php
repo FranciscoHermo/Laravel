37e65deca7b4fa6e61711701dfc11c2cb764489a
FranciscoHermo <francisco.hermosillo@cceo.com.mx>
2026-02-24 09:51:48 -0600

    Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,12 +18,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        $this->call(CategorySeeder::class);
-        $this->call(PostSeeder::class);
+        // $this->call(CategorySeeder::class);
+        // $this->call(PostSeeder::class);
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Post::factory(30)->create();
     }
 }
